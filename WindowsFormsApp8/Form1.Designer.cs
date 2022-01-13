32f94fabbc033b54523c146fe9a45444fc6f5930
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-13 16:35:56 +0900

    초기 커밋.

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnFile = new System.Windows.Forms.Button();
+            this.BtnStream = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 29F);
+            this.label1.Location = new System.Drawing.Point(26, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. 텍스트 파일 처리";
+            // 
+            // BtnFile
+            // 
+            this.BtnFile.Location = new System.Drawing.Point(59, 85);
+            this.BtnFile.Name = "BtnFile";
+            this.BtnFile.Size = new System.Drawing.Size(148, 103);
+            this.BtnFile.TabIndex = 1;
+            this.BtnFile.Text = "File 클래스";
+            this.BtnFile.UseVisualStyleBackColor = true;
+            this.BtnFile.Click += new System.EventHandler(this.BtnFile_Click);
+            // 
+            // BtnStream
+            // 
+            this.BtnStream.Location = new System.Drawing.Point(59, 222);
+            this.BtnStream.Name = "BtnStream";
+            this.BtnStream.Size = new System.Drawing.Size(148, 108);
+            this.BtnStream.TabIndex = 2;
+            this.BtnStream.Text = "StreamReader, StreamWriter 클래스";
+            this.BtnStream.UseVisualStyleBackColor = true;
+            this.BtnStream.Click += new System.EventHandler(this.BtnStream_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnStream);
+            this.Controls.Add(this.BtnFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnFile;
+        private System.Windows.Forms.Button BtnStream;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void BtnFile_Click(object sender, EventArgs e)
+        {
+            // 간단한 텍스트 파일 쓰기
+            string data = "Data 1\r\n Data 2 \r\n Data 3 \r\n";
+            File.WriteAllText("test.txt", data);
+            // 파일 클래스에 writealltext 라는 메소드가 있다.
+            // 파일 패스는 text.txt이고 내용은 data1이다.
+            //File.AppendAllText("test.txt", "data 2");
+            //File.AppendAllText("test.txt", "data 3");
+
+            // 텍스트 파일 읽기
+            string data2 = File.ReadAllText("test.txt");
+            MessageBox.Show(data2);
+        }
+
+
+        //연속적으로 데이터가 들어올때, 데이터가 많을때
+        private void BtnStream_Click(object sender, EventArgs e)
+        {
+            StreamWriter wr = new StreamWriter("streamdata.txt");
+            for (int i = 0; i < 100000; i++)
+            {
+                wr.WriteLine($"Data {i}");
+ }
+            wr.Close();
+
+          
+                  
+            }
+        }
+    }
+
